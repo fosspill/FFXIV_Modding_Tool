19c20b625f22dc15be1eacf19ce8ad9d388a9102
Ivanka Heins <ivanka@tuta.io>
2019-08-14 10:13:35 +0200

    Move validation functions to its own class

---
+++ FFXIV_Modding_Tool/Validation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace FFXIV_Modding_Tool
+{
+    public class Validation
+    {
+        public Validation()
+        {
+        }
+
+        public bool ValidGameDirectory(DirectoryInfo directory)
+        {
+            if (!directory.Exists || directory.GetFiles("*.index").Length == 0)
+                return false;
+            return true;
+        }
+
+        public bool ValidBackupDirectory(DirectoryInfo directory)
+        {
+            if (!directory.Exists)
+                return false;
+            return true;
+        }
+
+        public bool ValidGameConfigDirectory(DirectoryInfo directory)
+        {
+            if (!directory.Exists || directory.GetFiles("FFXIV*.cfg").Length == 0)
+                return false;
+            return true;
+        }
+    }
+}
