0d217e3f99547a777eeeb39e29bca159df4acd15
Ivanka Heins <shinnova@users.noreply.github.com>
2019-10-23 18:20:19 +0200

    Retrieve racial data of textures

--- FFXIV_Modding_Tool/Export.cs
+++ FFXIV_Modding_Tool/Export.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using xivModdingFramework.General.Enums;
 using xivModdingFramework.Items.Enums;
+using xivModdingFramework.Items.Categories;
 using xivModdingFramework.Items.DataContainers;
+using FFXIV_Modding_Tool.Configuration;
 using FFXIV_Modding_Tool.Search;
 
 namespace FFXIV_Modding_Tool.Exporting
@@ -8,10 +11,21 @@ namespace FFXIV_Modding_Tool.Exporting
     public class Export
     {
         MainClass main = new MainClass();
+        Config config = new Config();
         public void GetExportInfo(GameSearch.ItemInfo item)
         {
             if (int.TryParse(item.name, out int modelId))
                 item = LoadModelData(modelId, item);
+            Dictionary<string, List<XivRace>> availableRaces = new Dictionary<string, List<XivRace>>();
+            main.PrintMessage("Retrieving racial data...");
+            availableRaces["Textures"] = GetTextureRaces(item);
+            availableRaces["Model"] = GetModelRaces(item);
+            foreach (KeyValuePair<string, List<XivRace>> allRaces in availableRaces)
+            {
+                main.PrintMessage(allRaces.Key);
+                foreach (XivRace race in allRaces.Value)
+                    main.PrintMessage(race.GetDisplayName());
+            }
         }
 
         GameSearch.ItemInfo LoadModelData(int modelId, GameSearch.ItemInfo item)
@@ -86,6 +100,63 @@ namespace FFXIV_Modding_Tool.Exporting
                 };
             }
             return itemInfo;
+        }
+
+        List<XivRace> GetTextureRaces(GameSearch.ItemInfo item)
+        {
+            List<XivRace> races = new List<XivRace>();
+            XivLanguage gameLanguage = XivLanguages.GetXivLanguage(config.ReadConfig("Language"));
+            if (item.category.Equals("Gear"))
+            {
+                Gear gear = new Gear(MainClass._indexDirectory, gameLanguage);
+                var gearItem = new XivGear{
+                    Name = item.name,
+                    Category = item.category,
+                    ItemCategory = item.itemCategory,
+                    ItemSubCategory = item.itemSubCategory,
+                    DataFile = item.dataFile,
+                    ModelInfo = item.primaryModelInfo,
+                    SecondaryModelInfo = item.secondaryModelInfo,
+                    IconNumber = item.iconNumber,
+                    EquipSlotCategory = item.equipSlotCategory
+                };
+                var getRaces = gear.GetRacesForTextures(gearItem, item.dataFile);
+                getRaces.Wait();
+                races.AddRange(getRaces.Result);
+            }
+            else if (item.category.Equals("Companions"))
+                races.Add(XivRace.DemiHuman);
+            else if (item.category.Equals("Character"))
+            {
+                if (item.itemCategory.Equals("Face_Paint") || item.itemCategory.Equals("Equip_Decals"))
+                    races.Add(XivRace.All_Races);
+                else
+                {
+                    Character character = new Character(MainClass._indexDirectory, gameLanguage);
+                    var charaItem = new XivCharacter{
+                    Name = item.name,
+                    Category = item.category,
+                    ItemCategory = item.itemCategory,
+                    ItemSubCategory = item.itemSubCategory,
+                    DataFile = item.dataFile,
+                    ModelInfo = item.primaryModelInfo
+                    };
+                    var charaRaceAndNumberDictionary = character.GetRacesAndNumbersForTextures(charaItem);
+                    foreach (var racesAndNumber in charaRaceAndNumberDictionary.Result)
+                        races.Add(racesAndNumber.Key);
+                }
+            }
+            else if (item.category.Equals("UI"))
+                races.Add(XivRace.All_Races);
+            else if (item.category.Equals("Housing"))
+                races.Add(XivRace.All_Races);
+            return races;
+        }
+
+        List<XivRace> GetModelRaces(GameSearch.ItemInfo item)
+        {
+            List<XivRace> races = new List<XivRace>();
+            return races;
         }
     }
 }
