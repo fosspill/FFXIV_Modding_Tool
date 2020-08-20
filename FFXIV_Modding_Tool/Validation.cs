23013903c847b498cdad47bbfcc5958845737f14
fosspill <b9ac4a2f-45b5-4551-9fe8-2083f56bc2f2@anonaddy.me>
2020-08-20 19:09:54 +0200

    Add using System.Collections.Generic

--- FFXIV_Modding_Tool/Validation.cs
+++ FFXIV_Modding_Tool/Validation.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.IO;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using xivModdingFramework.General.Enums;
 using xivModdingFramework.Helpers;
