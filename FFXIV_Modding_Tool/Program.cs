37f5b1c2cfd85e01aede5d695e358168ff229786
Ivanka Heins <shinnova@users.noreply.github.com>
2021-08-10 19:49:51 +0200

    Credit TexTools contributors

--- FFXIV_Modding_Tool/Program.cs
+++ FFXIV_Modding_Tool/Program.cs
@@ -13,6 +13,10 @@
 // 
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Credit to the TexTools UI contributors for inspiring and 
+// influencing a lot of the code.
+// They can be found here: >https://github.com/TexTools/FFXIV_TexTools_UI>
 
 using System;
 using System.IO;
